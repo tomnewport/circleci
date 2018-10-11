f0ad4d594bbe56aaf62e19c8970a03a6538f64f3
Tom Newport <tom.newport@nanoporetech.com>
2018-10-11 15:16:19 +0100

    Add a docstring to the function

--- thing/__init__.py
+++ thing/__init__.py
@@ -1,4 +1,7 @@
 def recursive_sum(input_data):
+    """
+    Recursively sum elements of a list, or return float value of anything else
+    """
     if isinstance(input_data, list):
         return sum([recursive_sum(el) for el in input_data])
     else:
