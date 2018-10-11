3ed858fb4217a9f071fee1fbddb462c0514941dc
Tom Newport <tom.newport@nanoporetech.com>
2018-10-11 15:21:42 +0100

    Add a docstring and fix flow

--- thing/__init__.py
+++ thing/__init__.py
@@ -1,8 +1,14 @@
+"""
+Thingy!
+"""
+
 def recursive_sum(input_data):
     """
     Recursively sum elements of a list, or return float value of anything else
     """
+    value = None
     if isinstance(input_data, list):
-        return sum([recursive_sum(el) for el in input_data])
+        value = sum([recursive_sum(el) for el in input_data])
     else:
-        return float(input_data)
+        value = float(input_data)
+    return value
